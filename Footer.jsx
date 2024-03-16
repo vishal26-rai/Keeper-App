f73e99da7e4f5a75d804443192438463ed23bbd1
vishal26-rai <163676767+vishal26-rai@users.noreply.github.com>
2024-03-17 01:10:40 +0530

    Add files via upload

---
+++ Footer.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Footer(){
+    // const year = new Date.getfullYear();
+    const year = 2024;
+    return(
+        <footer>
+            <p>copyright ⓒ {year}</p>
+        </footer>
+    );
+}
+export default Footer;
---
+++ Header.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+function Header() {
+    return(
+        <header>
+            <h1>
+                keeper
+            </h1>
+            <h6>keep your notes on tips</h6>
+        </header>
+    )
+}
+export default Header;
